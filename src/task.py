0d177f5524f27cc71bdb288121b484251828c874
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2021-04-18 13:48:20 -0400

    Add fix actions

--- src/task.py
+++ src/task.py
@@ -16,15 +16,51 @@ class Task(MetaTaskInterface):
     fail_summary = "Files not formatted correctly."
     fail_text = ""
     subscription_level = SubscriptionLevels.FREE
-    actions = None
+    _actions = [
+        {
+            "label": "Fix",
+            "identifier": "fix",
+            "description": "Fix formatting issues."
+        }
+    ]
 
     def execute(self, github_body, settings) -> bool:
+
+        fix_options = {
+            'enabled': False,
+            'message': 'style: formatting code with %s' % self.name
+        }
+
+        head_branch = None
+        base_branch = None
+
+        # A request to fix formatting has been made
+        if (
+            github_body.get("githaxs", {}).get("full_event")
+            == "check_run.requested_action"
+            and github_body.get("requested_action", {}).get("identifier", "")
+            == "fix"
+        ):
+            fix_options['enabled'] = True
+            head_branch = github_body['check_run']['pull_requests'][0]['head']['ref']
+            base_branch = github_body['check_run']['pull_requests'][0]['base']['ref']
+
+        else:
+            head_branch = github_body['pull_request']['head']['ref']
+            base_branch = github_body['repository']['default_branch']
+
+        subscription_includes_autofix = github_body.get('githaxs').get('subscription_level') >= SubscriptionLevels.GROWTH
+
+        if subscription_includes_autofix and settings.get('auto_fix') == True:
+            fix_options['enabled'] = True
+
         code_check = CodeCheck(
             token=github_body.get('githaxs').get('token'),
-            branch = github_body.get('pull_request', {}).get('head', {}).get('ref'),
-            default_branch = github_body.get('repository', {}).get('default_branch'),
+            branch = head_branch,
+            default_branch = base_branch,
             full_repo_name = github_body.get('repository', {}).get('full_name'),
-            source_script_path = '%s/task.sh' % os.path.dirname(__file__)
+            source_script_path = '%s/task.sh' % os.path.dirname(__file__),
+            fix_options=fix_options
 
         )
 
@@ -32,4 +68,16 @@ class Task(MetaTaskInterface):
 
         self.fail_text = code_check.fail_text
 
+        # If check passed, no extra actions needed
+        if code_check.result:
+            self._actions = None
+
         return code_check.result
+
+    @property
+    def actions(self):
+        return self._actions
+
+    @actions.setter
+    def actions(self, actions):
+        self._actions = actions
