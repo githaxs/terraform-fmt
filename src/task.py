c4580d5bcf7341ca3a8b8ff254902146543d192e
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2021-08-08 20:11:12 -0400

    feat: upgrade task to use new base task

--- src/task.py
+++ src/task.py
@@ -1,10 +1,9 @@
 import os
 
-from task_interfaces import MetaTaskInterface, SubscriptionLevels
-from code_check import CodeCheck
+from task_interfaces import TaskInterface, SubscriptionLevels, TaskTypes
 
 
-class Task(MetaTaskInterface):
+class Task(TaskInterface):
     """
     Ensure all Terraform files are formatted correctly
     """
@@ -16,68 +15,11 @@ class Task(MetaTaskInterface):
     fail_summary = "Files not formatted correctly."
     fail_text = ""
     subscription_level = SubscriptionLevels.FREE
-    _actions = [
-        {
-            "label": "Fix",
-            "identifier": "fix",
-            "description": "Fix formatting issues."
-        }
-    ]
+
+    actions = None
+    type = TaskTypes.CODE_FORMAT
+    command = "terraform fmt -no-color -list=false"
+    file_filters = ".*.(tf|tfvars)$"
 
     def execute(self, github_body, settings) -> bool:
-
-        fix_options = {
-            'enabled': False,
-            'message': 'style: formatting code with %s' % self.name
-        }
-
-        head_branch = None
-        base_branch = None
-
-        # A request to fix formatting has been made
-        if (
-            github_body.get("githaxs", {}).get("full_event")
-            == "check_run.requested_action"
-            and github_body.get("requested_action", {}).get("identifier", "")
-            == "fix"
-        ):
-            fix_options['enabled'] = True
-            head_branch = github_body['check_run']['pull_requests'][0]['head']['ref']
-            base_branch = github_body['check_run']['pull_requests'][0]['base']['ref']
-
-        else:
-            head_branch = github_body['pull_request']['head']['ref']
-            base_branch = github_body['repository']['default_branch']
-
-        subscription_includes_autofix = github_body.get('githaxs').get('subscription_level') >= SubscriptionLevels.GROWTH
-
-        if subscription_includes_autofix and settings.get('auto_fix') == True:
-            fix_options['enabled'] = True
-
-        code_check = CodeCheck(
-            token=github_body.get('githaxs').get('token'),
-            branch = head_branch,
-            default_branch = base_branch,
-            full_repo_name = github_body.get('repository', {}).get('full_name'),
-            source_script_path = '%s/task.sh' % os.path.dirname(__file__),
-            fix_options=fix_options
-
-        )
-
-        code_check.execute()
-
-        self.fail_text = code_check.fail_text
-
-        # If check passed, no extra actions needed
-        if code_check.result:
-            self._actions = None
-
-        return code_check.result
-
-    @property
-    def actions(self):
-        return self._actions
-
-    @actions.setter
-    def actions(self, actions):
-        self._actions = actions
+        pass
