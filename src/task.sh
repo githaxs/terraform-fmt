3be8d1e143055d0998ab065b54020681f7852213
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2021-10-26 07:07:43 -0400

    fix: add task.sh

---
+++ src/task.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+run() {
+    if [[ $1 =~ .*.tf$ ]]; then
+        terraform fmt -no-color -list=false $1
+    fi
+}
+
+task() {
+    echo "Running Terraform Format"
+
+    foreach_changed_file_loop run
+}
